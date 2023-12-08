66185f0ea2da88e0df95326d4653c5556853f990
supernovatux <thulashitharan.d@gmail.com>
2023-12-08 14:53:26 +0530

    init

---
+++ src/microgrid/main.py
@@ -0,0 +1,10 @@
+from pymgrid.modules import (
+    BatteryModule,
+    LoadModule,
+    RenewableModule,
+    GridModule)
+from pymgrid import Microgrid
+import numpy as np
+import pandas as pd
+
+np.random.seed(0)
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+from microgrid import __version__
+
+
+def test_version():
+    assert isinstance(__version__, str)
